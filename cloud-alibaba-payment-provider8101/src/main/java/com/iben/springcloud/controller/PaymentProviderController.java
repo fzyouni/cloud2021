10fe7326daad35b6702c2e329dba858c0fdf3e83
fz <fzyouni@163.com>
2021-01-28 18:00:19 +0800

    演示sentinel 降级 限流等功能

--- cloud-alibaba-payment-provider8101/src/main/java/com/iben/springcloud/controller/PaymentProviderController.java
+++ cloud-alibaba-payment-provider8101/src/main/java/com/iben/springcloud/controller/PaymentProviderController.java
@@ -1,6 +1,9 @@
 package com.iben.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,5 +17,24 @@ public class PaymentProviderController {
     @GetMapping("testA")
     public String testA() {
         return "this is testA";
+    }
+
+    /**
+     * 测试sentinel的热点限流
+     *
+     * @param key
+     * @return
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        System.out.println("testHotKey");
+        return "----testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        System.out.println("deal_testHotKey");
+        return "----deal_testHotKey, o(╥﹏╥)o"; // sentinel的默认提示都是： Blocked by Sentinel (flow limiting)
     }
 }
